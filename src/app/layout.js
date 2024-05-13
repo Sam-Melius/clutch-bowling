ca84ccd155440566a90d5929ef704ed462a40fc2
gabewolford <gabewolford@gmail.com>
2024-05-13 11:02:09 -0700

    installed google and vercel analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,8 @@ import TheHeader from "./components/TheHeader";
 import TheFooter from "./components/TheFooter";
 import { roboto } from "./styles/fonts/fonts";
 import AOSLoader from "./utils/AOSLoader";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata = {
   title: "Clutch Bowling",
@@ -20,6 +22,8 @@ export default function RootLayout({ children }) {
         {children}
         <TheFooter />
         <AOSLoader />
+        <Analytics />
+        <GoogleAnalytics gaId="G-Q6EGTNFW63" />
       </body>
     </html>
   );
