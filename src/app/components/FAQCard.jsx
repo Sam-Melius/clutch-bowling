e5101c7eec9e9b91064fc17ba8aecd5925e07ebd
gabewolford <gabewolford@gmail.com>
2024-05-13 10:30:10 -0700

    added animation to faq card

--- src/app/components/FAQCard.jsx
+++ src/app/components/FAQCard.jsx
@@ -1,18 +1,23 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { GoPlus } from "react-icons/go";
 import { AiOutlineMinus } from "react-icons/ai";
 
 export default function FAQCard({ faq }) {
   const [showAnswer, setShowAnswer] = useState(false);
 
+  // Ensure initial state matches on the client and server
+  useEffect(() => {
+    setShowAnswer(false);
+  }, []);
+
   const toggleAnswer = () => {
     setShowAnswer(!showAnswer);
   };
 
   return (
-    <div className="flex flex-col gap-4 border-b border-gray-400 py-8 w-full lg:w-2/3 xl:w-3/4 mx-auto">
+    <div className="flex flex-col gap-4 border-b border-gray-400 pt-8 pb-4 w-full lg:w-2/3 xl:w-3/4 mx-auto">
       <div className="flex flex-row items-start justify-between gap-5">
         <button
           className="w-fit text-xl lg:text-2xl xl:text-3xl text-left"
@@ -21,13 +26,31 @@ export default function FAQCard({ faq }) {
           {faq.question}
         </button>
         <button className="text-3xl lg:text-4xl" onClick={toggleAnswer}>
-          {showAnswer ? <AiOutlineMinus /> : <GoPlus />}
+          {showAnswer ? (
+            <AiOutlineMinus className="animate__animated animate__rotateIn animate__faster" />
+          ) : (
+            <GoPlus />
+          )}
         </button>
       </div>
 
-      {showAnswer && (
-        <p className="text-clutchBlue-300 lg:text-lg pr-12">{faq.answer}</p>
-      )}
+      <div
+        style={{
+          maxHeight: showAnswer ? "1000px" : "0",
+          overflow: "hidden",
+          transition: showAnswer
+            ? "max-height 1s ease-in"
+            : "max-height .3s ease-in",
+        }}
+      >
+        <p
+          className={`text-clutchBlue-300 lg:text-lg pr-12 overflow-hidden transition-all ${
+            showAnswer ? "max-h-fit pb-4" : "max-h-0"
+          } `}
+        >
+          {faq.answer}
+        </p>
+      </div>
     </div>
   );
 }
