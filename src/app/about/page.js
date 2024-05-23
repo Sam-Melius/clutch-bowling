6820c4e3d705bf067b371040596c949a1dc5f124
gabewolford <gabewolford@gmail.com>
2024-05-23 09:43:02 -0700

    updated about page hero video

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,11 +1,6 @@
-import LinkButton from "../components/Buttons/LinkButton";
-import HeadingAndBreadcrumbs from "../components/HeadingAndBreadcrumbs";
-import { FaArrowRight } from "react-icons/fa";
-import fireballEffect from "../../../public/videos/fireball.gif";
-import Image from "next/image";
-import GradientSection from "../components/GradientSection";
 import GridlinesSection from "../components/GridlinesSection";
 import AllFeaturesSection from "../components/AllFeaturesSection";
+import AboutHeroVideo from "../components/AboutHeroVideo";
 
 export const metadata = {
   title: "Clutch Bowling | About",
@@ -37,42 +32,7 @@ export const metadata = {
 export default function AboutPage() {
   return (
     <main>
-      <HeadingAndBreadcrumbs pageHeading="About" />
-
-      <section>
-        <div className="flex flex-col h-full w-full">
-          <div className="flex flex-col lg:flex-row mx-auto lg:items-center justify-between gap-5 xl:gap-0 py-10 px-5 md:px-10 lg:p-10 2xl:p-28">
-            <div
-              className="flex flex-col gap-5 lg:w-1/2 xl:min-w-[530px] 2xl:min-w-[950px]"
-              // data-aos="fade-up"
-              // data-aos-duration="1000"
-              // data-aos-once="true"
-            >
-              <h4 className="text-7xl lg:text-6xl xl:text-7xl 2xl:text-9xl">
-                Built to impress.
-              </h4>
-              <p className="2xl:text-xl">
-                With precise tracking and crisp imaging we strive to immerse
-                bowlers of all ages in our system. Clutch Bowling is a new kind
-                of bowling experience that combines vibrant graphics and motion
-                tracking with the sport of bowling.
-              </p>
-              <LinkButton
-                variant="secondary"
-                linkText="See all features"
-                linkTo="#all-features"
-                icon={<FaArrowRight />}
-              />
-            </div>
-            <Image
-              src={fireballEffect}
-              alt="Fireball effect"
-              className="w-full lg:w-1/2 lg:p-10 2xl:p-16"
-              unoptimized
-            />
-          </div>
-        </div>
-      </section>
+      <AboutHeroVideo />
 
       <AllFeaturesSection />
 
---
+++ src/app/components/AboutHeroVideo.jsx
@@ -0,0 +1,51 @@
+import LinkButton from "./Buttons/LinkButton";
+import { FaArrowRight } from "react-icons/fa";
+import HeadingAndBreadcrumbs from "./HeadingAndBreadcrumbs";
+
+export default function AboutVideo() {
+  return (
+    <section className="flex h-svh w-full relative">
+      <video
+        autoPlay
+        muted
+        loop
+        className="w-full h-full object-cover"
+        preload="auto"
+        playsInline
+      >
+        <source src="/videos/clutch-bowling-demo.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      <div className="absolute -z-30 w-full h-full grid grid-cols-2 md:grid-cols-6 lg:grid-cols-12 place-items-center bg-primaryDark"></div>
+
+      <div className="absolute grid h-full w-full">
+        <HeadingAndBreadcrumbs pageHeading="About" />
+        <div className="grid w-full md:w-2/3 lg:w-1/2 place-items-end justify-items-start px-5 py-10 md:p-10">
+          <div
+            className="col-span-full flex flex-col gap-5"
+            data-aos="fade-up"
+            data-aos-duration="1000"
+            data-aos-once="true"
+          >
+            <h4 className="text-7xl md:text-8xl lg:text-8xl xl:text-9xl">
+              Built to impress.
+            </h4>
+            <p className="2xl:text-xl">
+              With precise tracking and crisp imaging we strive to immerse
+              bowlers of all ages in our system. Clutch Bowling is a new kind of
+              bowling experience that combines vibrant graphics and motion
+              tracking with the sport of bowling.
+            </p>
+            <LinkButton
+              variant="secondary"
+              linkText="See all features"
+              linkTo="#all-features"
+              icon={<FaArrowRight />}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
