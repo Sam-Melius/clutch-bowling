3e47ac0c950c929ae1b7374a33f270afb6eca5fc
Gabe Wolford <gabewolford@gmail.com>
2024-07-18 11:15:14 -0700

    created new contact button for preinstall form

---
+++ src/app/components/PreInstallContactButton.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { PopupButton } from "@typeform/embed-react";
+
+export default function PreInstallContactButton({ buttonText }) {
+  return (
+    <PopupButton id="dKjwIJeE" className="flex">
+      {buttonText}
+    </PopupButton>
+  );
+}
