bfcd8b052a1c1c517dc4d7877f900c6ed061a0e2
Gabe Wolford <gabewolford@gmail.com>
2024-07-18 12:48:42 -0700

    removed unused placeholder from old modal preview

--- src/app/components/LaneEffectCard.jsx
+++ src/app/components/LaneEffectCard.jsx
@@ -1,14 +1,6 @@
-"use client";
-
-import { useState } from "react";
 import Image from "next/image";
-import { FaEye } from "react-icons/fa";
-import placeholder from "../../../public/images/placeholder.png";
-import { MdOutlineClose } from "react-icons/md";
 
 export default function LaneEffectCard({ effectData }) {
-  const [showModal, setShowModal] = useState(false);
-
   return (
     <figure
       className="flex flex-col gap-2 relative"
@@ -16,13 +8,6 @@ export default function LaneEffectCard({ effectData }) {
       data-aos-duration="700"
       data-aos-once="true"
     >
-      {/* <button
-        className="absolute top-0 right-0 text-2xl z-10 bg-clutchBlue-800 hover:bg-clutchBlue-600 hover:cursor-pointer rounded-tr-md rounded-bl-md p-1 h-8 w-8"
-        onClick={() => setShowModal(true)}
-      >
-        <FaEye />
-      </button> */}
-
       <Image
         src={effectData.imageUrl}
         alt={effectData.name}
@@ -32,28 +17,9 @@ export default function LaneEffectCard({ effectData }) {
         height={effectData.height}
         width={effectData.width}
       />
-
       <figcaption className="flex flex-col font-medium md:text-lg">
         {effectData.name}
       </figcaption>
-
-      {/* {showModal && (
-        <div className="fixed inset-0 z-50">
-          <div className="rounded-lg relative">
-            <button
-              className="absolute top-0 right-0 text-2xl z-10 bg-clutchBlue-800 hover:bg-clutchBlue-600 hover:cursor-pointer rounded-tr-md rounded-bl-md p-1 h-8 w-8"
-              onClick={() => setShowModal(false)}
-            >
-              <MdOutlineClose />
-            </button>
-            <Image
-              src={placeholder}
-              alt="placeholder"
-              className="max-w-full h-auto rounded-lg object-cover"
-            />
-          </div>
-        </div>
-      )} */}
     </figure>
   );
 }
