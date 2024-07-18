c16f78ca924f3fba3e930df8335d6a7e7c86d462
Gabe Wolford <gabewolford@gmail.com>
2024-07-18 11:15:32 -0700

    updated components with new prop

--- src/app/components/Buttons/LinkButton.jsx
+++ src/app/components/Buttons/LinkButton.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import ContactButton from "../ContactButton";
+import PreInstallContactButton from "../PreInstallContactButton";
 
 export default function LinkButton({
   linkTo,
@@ -7,8 +8,9 @@ export default function LinkButton({
   icon,
   variant,
   isContact,
+  isPreInstallForm,
 }) {
-  if (variant === "primary" && !isContact) {
+  if (variant === "primary" && !isContact && !isPreInstallForm) {
     return (
       <Link
         href={linkTo}
@@ -18,7 +20,7 @@ export default function LinkButton({
         <div className="text-sm md:text-base">{icon}</div>
       </Link>
     );
-  } else if (variant === "secondary" && !isContact) {
+  } else if (variant === "secondary" && !isContact && !isPreInstallForm) {
     return (
       <Link
         href={linkTo}
@@ -28,7 +30,7 @@ export default function LinkButton({
         <div className="text-sm md:text-base">{icon}</div>
       </Link>
     );
-  } else if (variant === "black" && !isContact) {
+  } else if (variant === "black" && !isContact && !isPreInstallForm) {
     return (
       <Link
         href={linkTo}
@@ -56,6 +58,27 @@ export default function LinkButton({
     return (
       <div className="flex flex-row items-center gap-2 w-fit lg:text-xl leading-none pb-0.5 border-b lg:border-b-2 border-transparent hover:text-clutchBlue-500 text-black hover:border-clutchBlue-500 transition duration-300 font-medium">
         <ContactButton buttonText={linkText} />
+        <div className="text-sm md:text-base">{icon}</div>
+      </div>
+    );
+  } else if (variant === "primary" && isPreInstallForm) {
+    return (
+      <div className="flex flex-row items-center gap-2 w-fit lg:text-xl leading-none pb-0.5 border-b lg:border-b-2 border-transparent text-white hover:text-clutchBlue-500 hover:border-clutchBlue-500 transition duration-300 font-medium">
+        <PreInstallContactButton buttonText={linkText} />
+        <div className="text-sm md:text-base">{icon}</div>
+      </div>
+    );
+  } else if (variant === "secondary" && isPreInstallForm) {
+    return (
+      <div className="flex flex-row items-center gap-2 w-fit lg:text-xl leading-none pb-0.5 border-b lg:border-b-2 border-transparent text-clutchBlue-500 hover:text-white hover:border-white transition duration-300 font-medium">
+        <PreInstallContactButton buttonText={linkText} />
+        <div className="text-sm md:text-base">{icon}</div>
+      </div>
+    );
+  } else if (variant === "black" && isPreInstallForm) {
+    return (
+      <div className="flex flex-row items-center gap-2 w-fit lg:text-xl leading-none pb-0.5 border-b lg:border-b-2 border-transparent text-clutchBlue-500 hover:text-white hover:border-white transition duration-300 font-medium">
+        <PreInstallContactButton buttonText={linkText} />
         <div className="text-sm md:text-base">{icon}</div>
       </div>
     );
