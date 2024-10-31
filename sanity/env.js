8fe12f68ee02fe65c1bf1f5167eab5b4ed77101b
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2024-10-31 10:30:32 -0700

    added header2

--- sanity/env.js
+++ sanity/env.js
@@ -1,12 +1,7 @@
-// export const apiVersion =
-//   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-05-16'
-
-// export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-// export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
-// export const useCdn = false
-
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-30'
-export const dataset = 'production';
-export const projectId = 'mlqi9vxv';
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-05-16'
+
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const useCdn = false
+
