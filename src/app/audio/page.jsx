564f3500e2e1368bee404e51ba7af5296386f771
Sam Melius Oxford <samsjmelius@gmail.com>
2025-09-19 10:06:47 -0700

    added Audio solutions page

--- src/app/audio/page.jsx
+++ src/app/audio/page.jsx
@@ -1,21 +1,23 @@
-import AllFeaturesSection from "../components/AllFeaturesSection";
 import HeroVideoAlt from "../components/HeroVideoAlt";
 import TrianglesSection from "../components/TrianglesSection";
+import AudioFeaturesSection from "../components/AudioFeaturesSection";
 
 export default function AboutPage() {
   return (
     <main>
       <HeroVideoAlt
         videoUrl="/videos/clutch-bowling-demo.mp4"
-        mainHeading="Built to impress."
-        subheading="With precise tracking and crisp imaging we strive to immerse
-        bowlers of all ages in our system. Clutch Bowling is a new kind of
-        bowling experience that combines vibrant graphics and motion
-        tracking with the sport of bowling."
+        mainHeading="Audio That Enhances Play"
+        subheading="Bowling alleys aren’t quiet places—pins crash, balls roll, and crowds cheer. Delivering music, announcements, and interactive audio without overwhelming the natural sounds of bowling requires purpose-built solutions. Our professional audio systems are designed specifically for bowling environments, combining advanced acoustics and multi-zone control. Whether you’re upgrading a boutique center or outfitting a large entertainment complex, we deliver reliable, cost-effective audio that keeps your guests engaged and your operations seamless."
         linkTo="#all-features"
         linkText="See all features"
       />
-    <div>AUDIO SOLUTIONS</div>
+    <AudioFeaturesSection />
+    <TrianglesSection
+            heading="Have more questions?"
+            linkText="View our FAQs"
+            linkTo="/frequently-asked-questions"
+          />
     </main>
   );
 }
